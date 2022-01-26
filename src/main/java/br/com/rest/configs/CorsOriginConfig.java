f56bbbdd33e23550d02df474d364ae8fafd95754
Lelton Borges <leltondev@gmail.com>
2022-01-25 23:55:51 -0300

    CrossOrigin

---
+++ src/main/java/br/com/rest/configs/CorsOriginConfig.java
@@ -0,0 +1,18 @@
+package br.com.rest.configs;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class CorsOriginConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.
+                addMapping("/users/**").allowedMethods("GET")
+                .allowedOrigins("http://localhost:81", "http://localhost:8080");
+    }
+}
